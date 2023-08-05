7ed69727b2ba9ab75164b278a62e6f04eb43fcd9
Manuel rincon <manuelrincon37@gmail.com>
2023-08-04 20:38:36 -0400

    Html & bootstrap

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Categoria(models.Model):
+    """Modelo categoria"""
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    """Modelo productos"""
+
+    nombre = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    puntaje = models.FloatField()
+    categoria = models.ForeignKey(
+        Categoria, default=1, on_delete=models.CASCADE)
+
+    creado_en = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.nombre
--- productos/views.py
+++ productos/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-
+from .models import Producto
 # Create your views here.
+
+# /productos/
+
+
+def index(request):
+    productos = Producto.objects.all()
+    return render(
+        request,
+        'index.html',
+        context={'productos': productos}
+    )
