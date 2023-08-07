0820ceca7ffc7671d7c9218ec6e078e32cd4e155
Manuel rincon <manuelrincon37@gmail.com>
2023-08-06 23:21:15 -0400

    general commit

--- productos/urls.py
+++ productos/urls.py
@@ -1,13 +1,13 @@
 """ productos/urls """
 from django.urls import path
 from . import views
-
+app_name = 'productos'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('formulario', views.formulario, name='formulario'),
     path(
         '<int:producto_id>',
         views.detalle,
-        name='producto_detalle'
+        name='detalle'
     )
-
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from .forms import ProductoForm
 from .models import Producto
 # Create your views here.
 
@@ -16,10 +18,21 @@ def index(request):
 
 
 def detalle(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
     producto = Producto.objects.get(id=producto_id)
 
     return render(
         request,
         'detalle.html',
         context={'producto': producto}
+    )
+
+
+def formulario(request):
+    form = ProductoForm()
+
+    return render(
+        request,
+        'producto_form.html',
+        context={'form': form}
     )
